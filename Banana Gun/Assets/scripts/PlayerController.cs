df1b99f02daab806eaeb933dd3e40c10c72a8cc3
Ventura222 <josematrom.hotmail.com>
2015-04-18 21:27:32 +0200

    Adding things that i forgot

---
+++ Banana Gun/Assets/scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxVelocity = 10f;
+
+	private bool facingRight;
+	private Rigidbody2D rb2d;
+
+	private Animator anim;
+
+	void Awake(){
+		rb2d = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float move = Input.GetAxis ("Horizontal");
+
+		rb2d.velocity = new Vector2(move*maxVelocity , rb2d.velocity.y);
+
+		if(move > 0 && !facingRight)
+			Flip ();
+		else if(move < 0 && facingRight)
+			Flip ();
+
+		anim.SetFloat ("speed", Mathf.Abs(move));
+
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+
+		Vector3 theScale = transform.localScale;
+
+		theScale.x = theScale.x * -1;
+	}
+}
