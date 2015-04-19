eded19e9ccee601985526adb45410bd5d5e16c63
Ventura222 <josematrom.hotmail.com>
2015-04-19 16:11:08 +0200

    Player dead and spikes added

--- Banana Gun/Assets/scripts/FireClone.cs
+++ Banana Gun/Assets/scripts/FireClone.cs
@@ -6,11 +6,12 @@ public class FireClone : MonoBehaviour {
 	public GameObject fdsf;
 	public float forceFire = 10;
 	private bool fired;
-
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		fired = false;
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -25,14 +26,14 @@ public class FireClone : MonoBehaviour {
 			launchVector.z = 0;
 
 			//instantiate and add velocity
-			GameObject clone =  Instantiate(fdsf, launchVector.normalized + transform.position, Quaternion.identity) as GameObject;
+			GameObject clone =  Instantiate(fdsf, launchVector.normalized*0.5f + transform.position, Quaternion.identity) as GameObject;
 			Vector2 force = (Vector2)launchVector;
 
 			clone.GetComponent<Rigidbody2D>().AddForce (forceFire*force.normalized, ForceMode2D.Impulse);
 
 			Camera.main.GetComponent<MainCharacterFollow>().bazooka = clone.transform;
 
-
+			anim.SetTrigger("fire");
 			fired=true;
 
 		}
---
+++ Banana Gun/Assets/scripts/PlayerDead.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDead : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) 
+	{
+		if(collision.gameObject.tag == "Enemy")
+		{
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}
