17a3540792505edee6777973dc7f4e26a94eccb1
Ventura222 <josematrom.hotmail.com>
2015-04-20 22:20:43 +0200

    adding things

--- Banana Gun/Assets/scripts/LoadLevel.cs
+++ Banana Gun/Assets/scripts/LoadLevel.cs
@@ -5,14 +5,23 @@ public class LoadLevel : MonoBehaviour {
 
 	public int levelToLoad;
 
+	public float timeToWait = 35;
+
+	private float time;
 	// Use this for initialization
 	void Start () {
-	
+		time = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown (KeyCode.Space))
-			Application.LoadLevel (levelToLoad);
+
+		if(time>timeToWait)
+			if(Input.GetKeyDown (KeyCode.Space))
+				Application.LoadLevel (levelToLoad);
+
+		if(time<=timeToWait)
+			time += Time.deltaTime;
+
 	}
 }
